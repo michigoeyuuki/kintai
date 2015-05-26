f4bd19d54c5eceffd223491a2d0644f29c7bd056
michigoe <michigoe2551@gmail.com>
2015-05-26 17:33:29 +0900

    日付を表示させた

--- Kintai/Models/AttendanceMonth.cs
+++ Kintai/Models/AttendanceMonth.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 namespace Kintai.Models
 {
   public class AttendanceMonth
   {
     [Key]
+    [DisplayName("勤怠月")]
     public int Month { get; set; } //勤怠月
     public int UserCode { get; set; } //社員コード
 
