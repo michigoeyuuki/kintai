d192b23d92d538d5b335edec3a98b7528614c597
michigoe <michigoe2551@gmail.com>
2015-05-26 17:00:47 +0900

    余計な部分削除

--- Kintai/Controllers/KintaiController.cs
+++ Kintai/Controllers/KintaiController.cs
@@ -9,115 +9,76 @@ using Kintai.Models;
 
 namespace Kintai.Controllers
 {
-    public class KintaiController : Controller
+  public class KintaiController : Controller
+  {
+    private KintaiContext db = new KintaiContext();
+
+    //
+    // GET: /Kintai/
+    // 初期画面表示時
+
+    public ActionResult Index()
     {
-        private KintaiContext db = new KintaiContext();
-
-        //
-        // GET: /Kintai/
-
-        public ActionResult Index()
-        {
-            return View(db.AttendanceMonths.ToList());
-        }
-
-        //
-        // GET: /Kintai/Details/5
-
-        public ActionResult Details(int id = 0)
-        {
-            AttendanceMonth attendancemonth = db.AttendanceMonths.Find(id);
-            if (attendancemonth == null)
-            {
-                return HttpNotFound();
-            }
-            return View(attendancemonth);
-        }
-
-        //
-        // GET: /Kintai/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Kintai/Create
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(AttendanceMonth attendancemonth)
-        {
-            if (ModelState.IsValid)
-            {
-                db.AttendanceMonths.Add(attendancemonth);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(attendancemonth);
-        }
-
-        //
-        // GET: /Kintai/Edit/5
-
-        public ActionResult Edit(int id = 0)
-        {
-            AttendanceMonth attendancemonth = db.AttendanceMonths.Find(id);
-            if (attendancemonth == null)
-            {
-                return HttpNotFound();
-            }
-            return View(attendancemonth);
-        }
-
-        //
-        // POST: /Kintai/Edit/5
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(AttendanceMonth attendancemonth)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(attendancemonth).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(attendancemonth);
-        }
-
-        //
-        // GET: /Kintai/Delete/5
-
-        public ActionResult Delete(int id = 0)
-        {
-            AttendanceMonth attendancemonth = db.AttendanceMonths.Find(id);
-            if (attendancemonth == null)
-            {
-                return HttpNotFound();
-            }
-            return View(attendancemonth);
-        }
-
-        //
-        // POST: /Kintai/Delete/5
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            AttendanceMonth attendancemonth = db.AttendanceMonths.Find(id);
-            db.AttendanceMonths.Remove(attendancemonth);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            db.Dispose();
-            base.Dispose(disposing);
-        }
+      //とりあえず全件
+      //ログインしたユーザーのUserCodeと現在の年を検索条件で検索かける？
+      return View(db.AttendanceMonths.ToList());
     }
+
+    //
+    // GET: /Kintai/Create
+    //  新規登録初期表示
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    //
+    // POST: /Kintai/Create
+    //  新規登録画面　保存押下時
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(AttendanceMonth attendancemonth)
+    {
+      if (ModelState.IsValid)
+      {
+        db.AttendanceMonths.Add(attendancemonth);
+        db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+
+      return View(attendancemonth);
+    }
+
+    //
+    // GET: /Kintai/Edit/5
+    // 編集画面初期表示
+    public ActionResult Edit(int id = 0)
+    {
+      AttendanceMonth attendancemonth = db.AttendanceMonths.Find(id);
+      if (attendancemonth == null)
+      {
+        return HttpNotFound();
+      }
+      return View(attendancemonth);
+    }
+
+    //
+    //  POST: /Kintai/Edit/5
+    //  編集画面　保存ボタン押下時
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(AttendanceMonth attendancemonth)
+    {
+      if (ModelState.IsValid)
+      {
+        db.Entry(attendancemonth).State = EntityState.Modified;
+        db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+      return View(attendancemonth);
+    }
+
+  }
 }
